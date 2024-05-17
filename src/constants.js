07f0fd71754d949d441548a5d81041a4b02dd318
mensafri <safrijawad1245@gmail.com>
2024-05-17 10:37:54 +0700

    change constant

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,21 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-  I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-  You'll learn how I built this portfolio you're currently playing through!`,
-  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+	"pc": `This is my PC. I work mostly with JavaScript/TypeScript these days.
+    I have developed several mobile applications using the React Native framework. I also enjoy working with Python, and PHP. I am proficient in frameworks like React.js, Next.js, React Native, and Laravel. Regardless of the programming language, I just love programming.
+    Here is my <a href="https://github.com/mensafri" target="_blank">Github</a>!`,
+	"cs-degree": `I am currently pursuing my Bachelor's degree in Informatics at Universitas Muhammadiyah Semarang, and I am expected to graduate in 2026. I'm proud of the progress I've made so far and look forward to completing my degree.`,
+	"sofa-table": `That's my sofa. I like to relax here and watch YouTube.
+  I enjoy watching content from various tech channels and keeping up with the latest in technology and programming.`,
+	"tv": `That's my TV. I've been watching tech YouTubers a lot recently like:
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-  library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+	"bed": `This is where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+	"resume": `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1R-PeCgzd26DR6kru4TFmM5ZhzEh7vCtm/view?usp=sharing" target="_blank">Check it out?</a>
+  Contact me at safrijawad1245@gmail.com if you have any interesting job opportunities!`,
+	"projects": `Info about this portfolio: It's made with the Kaboom.js library, which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within the canvas. I enjoy experimenting with different technologies and frameworks.`,
+	"library": `There are a lot of programming books on my shelves.
   I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+	"exit": `If you want to exit Syafrie's portfolio, just close the tab.`,
 };
